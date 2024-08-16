216a37fb21c9137ac34c60627ea078a8b81ce68d
zeroxc23 <116192239+zeroxc23@users.noreply.github.com>
2024-08-15 22:14:13 -0500

    update

--- Python/Diccionarios.py
+++ Python/Diccionarios.py
@@ -8,3 +8,41 @@ print(Diccionario["Nombre"])
 #Tambien hay otro meodo con get
 x=Diccionario.get("Edad")
 print(x)
+
+dict = {
+    "Nombre": "Luisa",
+    "Apellido": "Martinez",
+    "Edad": 35,
+    "Lenguajes": {"Python", "Swift", "Kotlin"},
+    1: 1.77
+}
+#Inserción
+dict["Calle"]="Calle Principal"
+print(dict)
+#Actualización
+dict["Nombre"]="Pepe"
+print(dict["Nombre"])
+#Eliminación
+del dict["Calle"]
+print(dict)
+
+#Metodos de los diccionarios
+#keys() devuelve una lista de las claves del diccionario
+print(dict.keys())
+#values() devuelve una lista de los valores del diccionario
+print(dict.values())
+#items() devuelve una lista de los pares clave:valor del diccionario
+print(dict.items())
+#clear() elimina todos los elementos del diccionario
+print(dict.clear())
+#copy() devuelve una copia del diccionario
+print(dict.copy())
+#fromkeys() devuelve un diccionario con las claves especificadas y los valores especificados
+print(dict.fromkeys("Python", "Java"))
+#get() devuelve el valor para la clave especificada. Si la clave no existe, devuelve el
+#valor especificado como segundo argumento
+print(dict.get("Nombre", "No existe"))
+#items() devuelve una lista de los pares clave:valor del diccionario
+print(dict.items())
+#keys() devuelve una lista de las claves del diccionario
+print(dict.keys())
---
+++ Python/POO.py
@@ -0,0 +1,88 @@
+#La Programación Orientada a Objetos (POO) en Python es un paradigma de programación que se basa en la creación 
+# y manipulación de objetos, los cuales son instancias de clases.ç
+#Ejemplo
+class Miclase:     #Esta es laforma en la que la clase se define
+    x=12           #Esto seria la propiedad de la clase
+
+z=Miclase() #Desde aqui es cuando se creea el objeto
+print(z.x) 
+#Los ejemplos anteriores son clases y 
+# objetos en su forma más simple, y 
+# no son realmente útiles en aplicaciones de la vida real.
+
+# Todas las clases tienen una función llamada __init__(), 
+# que siempre se ejecuta cuando la clase se está iniciando.
+
+#Se utiliza la función __init__() para asignar valosres a las 
+# propiedades del objeto u operaciones si es necesario.
+
+class Persona:
+    #Atributos de la clase Persona
+    def __init__(self, nombre, edad, direccion):
+        self.nombre = nombre
+        self.edad = edad
+        self.direccion = direccion
+       
+ #Ahora a mostrar los datos de la persona
+persona1=Persona("Luis",30,"Kr 15")
+print(persona1.edad)
+
+# La función __init__() se llama en las clases cada vez que se utiliza 
+# la clase para crear un nuevo objeto.
+
+
+#Ahora veremos la función __str__()
+#La función __str__() se utiliza para mostrar los datos de un objeto.
+class Persona1:
+    def __init__(self, nombre, edad, direccion):
+        self.nombre = nombre
+        self.edad = edad
+        self.direccion = direccion
+    def __str__(self):
+        return f"{self.nombre}({self.edad})"
+
+persona1=Persona1("Luis",30,"Kr 15")
+print(persona1) #Luis(30)
+
+#Ahora con un metodo
+class Persona:
+  def __init__(self, nombre, edad):
+    self.nombre = nombre
+    self.edad = edad
+
+  def myfunc(self):
+    print("Hola mi nombre es » "+ self.nombre,self.edad)
+
+p1 = Persona("Juan",36)
+#Otras funciones 
+#Modificar la propiedad de los objetos
+p1.edad=40
+print(p1.edad) #40
+#Borrar la propiedad de los objetos
+del p1.edad
+print(p1.edad) #Error
+#Borrar objetos
+del p1
+p1.myfunc()
+
+#Sentencia pass
+#La sentencia pass se utiliza para indicar que una función o un bloque de código no tiene
+# ninguna acción que realizar. Por ejemplo, si se define una función pero no se
+# especifica qué acciones realizar, se puede utilizar la sentencia pass para indicar
+# que la función no tiene ninguna acción que realizar.
+#Ejemplo de uso de la sentencia pass
+
+class MiClase:
+   pass
+
+
+
+
+#Nota: El parámetro self es una referencia a la instancia actual de la clase, y
+#se utiliza para acceder a variables que pertenecen a la clase.
+
+
+
+
+      
+         
